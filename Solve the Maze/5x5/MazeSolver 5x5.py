1a350b66325dbdbd68e149d7bfc6cdd8654a0532
R Srinath <srinath1412001@gmail.com>
2020-12-06 17:37:24 +0530

    5x5 mazesolver

--- Solve the Maze/5x5/MazeSolver 5x5.py
+++ Solve the Maze/5x5/MazeSolver 5x5.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-filename = '5x55'
+filename = '5x5'
 img = cv2.imread(filename+'.png')
 cv2.imshow('Maze', img)
 
@@ -9,13 +9,17 @@ cv2.imshow('Maze', img)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # Inverting tholdolding will give us a binary image with a white wall and a black background.
 ret, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY_INV) 
-cv2.imwrite(filename+'/1. Threshold1.jpg', thresh)
+cv2.imwrite(filename+ '/1. Threshold1.jpg', thresh)
 cv2.imshow('Threshold 1', thresh)
 
 # Contours
-#Complete this code
 
-dc = cv2.drawContours(dc, contours, 1, (0,0,0) , 5)
+contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+dc = cv2.drawContours(thresh, contours, 0, (255, 255, 255),2)
+cv2.imwrite(filename+'/2. Contours1.jpg', dc)
+cv2.imshow('Contours 1', dc)
+
+dc = cv2.drawContours(dc, contours, 1, (0,0,0) ,2)
 cv2.imwrite(filename+'/3. Contours2.jpg', dc)
 cv2.imshow('Contours 2', dc)
 
@@ -23,37 +27,29 @@ ret, thresh = cv2.threshold(dc, 240, 255, cv2.THRESH_BINARY)
 cv2.imwrite(filename+'/4. Threshold2.jpg', thresh)
 cv2.imshow('Threshold 2', thresh)
 
-ke = 19
+ke = 9
 kernel = np.ones((ke, ke), np.uint8)
 
 # Dilate
-
-# Complete this section
+dilation = cv2.dilate(thresh, kernel, iterations=2)
+cv2.imwrite(filename+'/5. Dilation.jpg', dilation)
+cv2.imshow('Dilation', dilation)
 
 # Erosion
-
-# Complete this
+erosion = cv2.erode(dilation, kernel, iterations=1)
+cv2.imwrite(filename+'/6. Erosion.jpg', erosion)
+cv2.imshow('Erosion', erosion)
 
 # Find differences between two images
+diff = cv2.absdiff(dilation, erosion)
+cv2.imwrite(filename+'/7. Difference.jpg', diff)
+cv2.imshow('Difference', diff)
 
-# Complete this section
-
-# splitting the channels of maze
-
-# Complete this
-
-# In order to display the solution on the original maze image, first divide the original maze into r, g, b components.
-# Now create a mask by inverting the diff image.
-# The bitwise and r and g components of the original maze using the mask created in the last step.
-# This step will remove the red and green components from the image portion of the maze solution.
-# The last one is to merge all the components and we will use the blue marked solution.
-
-# masking out the green and red colour from the solved path
-r = cv2.bitwise_and(r, r, mask=mask_inv)
-b = cv2.bitwise_and(b, b, mask=mask_inv)
-
-res = cv2.merge((b, g, r))
-cv2.imwrite(filename+'/9. SolvedMaze.jpg', res)
-cv2.imshow('Solved Maze', res)
+#Result
+contour, hierarchy = cv2.findContours(diff, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+dc = cv2.drawContours(img, contour, 0, (0,255,0),3)
+cv2.imwrite(filename+'/8. Result.jpg', dc)
+cv2.imshow('Solution', dc)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
